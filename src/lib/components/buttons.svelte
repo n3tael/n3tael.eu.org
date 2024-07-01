2f3ff7cc57019b673005855cb313262e96c5b675
n3tael <netael@proton.me>
2024-07-01 12:59:59 +0300

    Added new button

---
+++ src/lib/components/buttons.svelte
@@ -0,0 +1,21 @@
+<ul class="buttons">
+	<li><a href="https://bedohswe.eu.org/"><img src="/buttons/bedohswe.png" alt="Bʰedoh₂ swé" width="88" height="31"></a></li>
+	<li><a href="https://doomzday.bandcamp.com"><img src="/buttons/doomzday.png" alt="Doömzday" width="88" height="31"></a></li>
+	<li><a href="https://www.mozilla.org/firefox/"><img src="/buttons/firefox.png" alt="Mozialla Firefox" width="88" height="31"></a></li>
+	<li><a href="https://houl.floof.company/"><img src="/buttons/houl.png" alt="Houl" width="88" height="31"></a></li>
+	<li><a href="https://piclog.blue/"><img src="/buttons/piclog.png" alt="piclog" width="88" height="31"></a></li>
+	<li><a href="https://slonk.ing/"><img src="/buttons/slonk.apng" alt="slonk" width="88" height="31"></a></li>
+	<li><a href="https://vendicated.dev/"><img src="/buttons/ven.png" alt="Vendicated" width="88" height="31"></a></li>
+	<li><a href="/b"><img src="/buttons/n3tael.gif" alt="n3tael" width="88" height="31"></a></li>
+	<li><div id="add-button"><a href="/c">add your button!</a></div></li>
+</ul>
+
+<style lang="postcss">
+    div#add-button {
+        @apply border-2 border-dashed border-main-700 p-0.5 text-xs leading-[.73rem] w-[88px] h-[31px];
+    }
+
+    div#add-button a {
+        @apply text-main-600
+    }
+</style>
--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,23 +1,8 @@
+<script lang="ts">
+	import Buttons from './buttons.svelte';
+</script>
+
 <footer>
     <p>best viewed on desktop | <a href="/l">licensing</a> • <a href="https://github.com/n3tael/n3tael.eu.org">source code</a></p>
-    <ul class="buttons">
-        <li><a href="https://doomzday.bandcamp.com"><img src="/buttons/doomzday.png" alt="Doömzday" width="88" height="31"></a></li>
-        <li><a href="https://www.mozilla.org/firefox/"><img src="/buttons/firefox.png" alt="Mozialla Firefox" width="88" height="31"></a></li>
-        <li><a href="https://houl.floof.company/"><img src="/buttons/houl.png" alt="Houl" width="88" height="31"></a></li>
-        <li><a href="https://piclog.blue/"><img src="/buttons/piclog.png" alt="piclog" width="88" height="31"></a></li>
-        <li><a href="https://slonk.ing/"><img src="/buttons/slonk.apng" alt="slonk" width="88" height="31"></a></li>
-        <li><a href="https://vendicated.dev/"><img src="/buttons/ven.png" alt="Vendicated" width="88" height="31"></a></li>
-        <li><a href="/b"><img src="/buttons/n3tael.gif" alt="n3tael" width="88" height="31"></a></li>
-        <li><div id="add-button"><a href="/c">add your button!</a></div></li>
-    </ul>
+    <Buttons />
 </footer>
-
-<style lang="postcss">
-    div#add-button {
-        @apply border-2 border-dashed border-main-700 p-0.5 text-xs leading-[.73rem] w-[88px] h-[31px];
-    }
-
-    div#add-button a {
-        @apply text-main-600
-    }
-</style>
