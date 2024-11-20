9d797ce3167bb07debb3c1ce25f7b9a3e2bc81d2
n3tael <netael@proton.me>
2024-11-20 13:43:47 +0200

    new buttons ah

--- src/lib/components/buttons.svelte
+++ src/lib/components/buttons.svelte
@@ -1,12 +1,15 @@
 <ul class="buttons">
-	<li><a href="https://bedohswe.eu.org/"><img src="/buttons/bedohswe.png" alt="Bʰedoh₂ swé" width="88" height="31"></a></li>
+	<li><a href="https://bedohswe.eu.org/"><img src="/buttons/bedohswe.gif" alt="Bʰedoh₂ swé" width="88" height="31"></a></li>
 	<li><a href="https://doomzday.bandcamp.com/"><img src="/buttons/doomzday.png" alt="Doömzday" width="88" height="31"></a></li>
 	<li><a href="https://meex.lol/"><img src="/buttons/meexsite.gif" title="MeexSite" width="88" height="31"></a>
 	<li><a href="https://www.mozilla.org/firefox/"><img src="/buttons/firefox.png" alt="Mozialla Firefox" width="88" height="31"></a></li>
+  <li><a href="https://imhex.werwolv.net/"><img src="/buttons/imhex.png" alt="ImHex" width="88" height="31"></a></li>
 	<li><a href="https://houl.floof.company/"><img src="/buttons/houl.png" alt="Houl" width="88" height="31"></a></li>
 	<li><a href="https://piclog.blue/"><img src="/buttons/piclog.png" alt="piclog" width="88" height="31"></a></li>
 	<li><a href="https://pansangg.github.io/"><img src="/buttons/pansangg.gif" title="pansangg" width="88" height="31"></a></li>
+  <li><a href="https://pixtaded.net/"><img src="/buttons/pixtaded.png" title="pixtaded" width="88" height="31"></a></li>
 	<li><a href="https://slonk.ing/"><img src="/buttons/slonk.apng" alt="slonk" width="88" height="31"></a></li>
+  <li><a href="https://rust-lang.org/"><img src="/buttons/rust.gif" alt="Rust" width="88" height="31"></a></li>
 	<li><a href="https://vendicated.dev/"><img src="/buttons/ven.png" alt="Vendicated" width="88" height="31"></a></li>
 	<li><a href="/b"><img src="/buttons/n3tael.gif" alt="n3tael" width="88" height="31"></a></li>
 	<li><div id="add-button"><a href="/c">add your button!</a></div></li>
