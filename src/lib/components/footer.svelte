2c508886ffc258ee27715ecb73a3f026db027b37
n3tael <netael@proton.me>
2025-04-14 19:07:48 +0300

    yarn update & fried webring

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import Buttons from './buttons.svelte';
+  import Webrings from './webrings.svelte';
 </script>
 
 <footer>
     <p>best viewed on desktop | <a href="/l">licensing</a> • <a href="https://github.com/n3tael/n3tael.eu.org">source code</a></p>
     <Buttons />
+    <Webrings />
 </footer>
---
+++ src/lib/components/webrings.svelte
@@ -0,0 +1,19 @@
+<ul id="webrings">
+	<li>
+    <a href="https://meex.lol/api/wr?p=n3tael.eu.org">←</a>
+    <a href="https://meex.lol/fried_webring">
+      <img src="/webrings/fried.png" width="120px" alt="fried webring">
+    </a>
+    <a href="https://meex.lol/api/wr?n=n3tael.eu.org">→</a>
+  </li>
+</ul>
+
+<style lang="postcss">
+    ul#webrings {
+      @apply flex flex-wrap gap-4 justify-center mt-2;
+    }
+   
+    ul#webrings li {
+      @apply flex gap-x-2 items-center;
+    }
+</style>
